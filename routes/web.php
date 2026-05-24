21b01c8797027282bc737a5cf1c5da78ac23fab8
Sohom Sarker <110902277+ArkaSarker@users.noreply.github.com>
2026-05-24 15:52:44 +0930

    created logic for cart, checkout page and simple structure

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\OrderController;
+
 Route::get('/', [ProductController::class, 'index']);
 
 Route::get('/products/{id}',
